64a0aa81c9699205592d9cb7c7071ef720cfc53e
balaji1610 <balajiece1610@gmail.com>
2022-03-02 19:04:29 +0530

    Array Method

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
-console.log("Hello");
-console.log("Hello");
+//-----------  Array --------------   ST
+
+const array_01 = ["Alex", "Balaji", "Chitra"];
+
+console.log(array_01[0]);
+
+console.log(array_01.length);
+
+//--- Add Methods
+const array_02 = ["Rio", "Denver", "Raqul"];
+const array_02a = ["tiger", "bear", "monkey"];
+const array_02b = ["Rio", "Denver", "Raqul"];
+const array_02c = ["tiger", "bear", "monkey"];
+
+array_02.push("Professor");
+
+array_02a.unshift("lion");
+
+array_02b.pop();
+array_02c.shift();
+
+console.log(array_02);
+
+console.log(array_02a);
+console.log(array_02b);
+console.log(array_02c);
+
+const text = "Hello Balaji What Are hell Doing ?";
+
+console.log(text.indexOf("Balaji"));
+console.log(text.includes("Balaji"));
+console.log(text.includes("Alex"));
+
+//-----------  End --------------   End
