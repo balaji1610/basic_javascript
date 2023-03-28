e93f9ea2a5b5da1b60b07c6bded6ba8d7522af5f
balaji1610 <balajiece1610@gmail.com>
2023-03-28 12:06:05 +0530

    cw8k_all_star_problem

--- Competitiveprogramming/Codewars/8Kseries/Codewars_8k_problems.js
+++ Competitiveprogramming/Codewars/8Kseries/Codewars_8k_problems.js
@@ -1,1 +1,18 @@
-console.log("Balaji");
+//-------->1<--------- All Star Code Challenge #18
+//link https://www.codewars.com/kata/5865918c6b569962950002a1
+
+function stringCount(str, i) {
+  return str.split("").filter((x) => x == i).length;
+}
+
+console.log(stringCount("", "z"));
+
+//2^nd method
+
+function secondStringCount(str, i) {
+  const emptyarray = [];
+  const output = str.split("").map((x) => (x == i ? emptyarray.push(x) : null));
+  return emptyarray.length;
+}
+
+console.log(secondStringCount("Hello", "l"));
